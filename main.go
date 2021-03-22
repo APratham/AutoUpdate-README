75b8ae56d2b8f188aa8c2773785d5d2e4da29cad
APratham <a.pratham1998@gmail.com>
2021-03-22 09:16:06 +0530

    One last try

--- main.go
+++ main.go
@@ -29,9 +29,9 @@ func makeReadme(filename string) error {
 
     // Whisk together static and dynamic content until stiff peaks form
     hello := "### Hello! I’m Victoria Drake. 👋\n\nI’m a software developer at 💜 and Director of Engineering at work. I build my skill stack in public and share open source knowledge through the words I’ve written on [victoria.dev](https://victoria.dev). I hope to encourage people to learn openly and fearlessly, with wild child-like abandon."
-  	quote := fact[content]
+  	// quote := fact[content]
   	updated := "<sub>Last updated by magic on " + date + ".</sub>"
-  	data := fmt.Sprintf("%s\n\n%s\n\n%s\n", hello, quote, updated)
+  	data := fmt.Sprintf("%s\n\n\n\n%s\n", hello, updated)
 
 	  // Prepare file with a light coating of os
 	  file, err := os.Create(filename)
