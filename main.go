39b1b07eb59397584d7c3eeda57f3e8dbac52613
APratham <a.pratham1998@gmail.com>
2021-03-23 17:48:29 +0530

    Made some changes

--- main.go
+++ main.go
@@ -12,24 +12,31 @@ import (
 
 func makeReadme(filename string) error {
 
-    // Read entire file content, giving us little control but
-    // making it very simple. No need to close the file.
-    content, err := ioutil.ReadFile("facts.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    // Convert []byte to string and print to screen
-    // text := string(content)
-    // fmt.Println(text)
 
     rand.Seed(time.Now().UnixNano())
+    randsource := rand.NewSource(time.Now().UnixNano())
+
     fact := rand.Intn(len(content))
     date := time.Now().Format("2 Jan 2006")
 
+
+                randgenerator := rand.New(randsource)
+                firstLoc := randgenerator.Intn(10)
+                fact := ""
+
+      // Read entire file content, giving us little control but
+      // making it very simple. No need to close the file.
+
+        dat, err := ioutil.ReadFile("pods_array.txt")
+        if err == nil {
+                ascii := string(dat)
+                splt := strings.Split(ascii, "\n")
+    fact = splt[firstLoc]
+
+
     // Whisk together static and dynamic content until stiff peaks form
     hello := "### Hello! I’m Victoria Drake. 👋\n\nI’m a software developer at 💜 and Director of Engineering at work. I build my skill stack in public and share open source knowledge through the words I’ve written on [victoria.dev](https://victoria.dev). I hope to encourage people to learn openly and fearlessly, with wild child-like abandon."
-  	quote := "⚡ Fun fact: " + fact[content]
+  	quote := "⚡ Fun fact: " + fact
   	updated := "<sub>Last updated by magic on " + date + ".</sub>"
   	data := fmt.Sprintf("%s\n\n\n\n%s\n", hello, updated)
 
@@ -38,7 +45,7 @@ func makeReadme(filename string) error {
 	  if err != nil {
 		   return err
 	   }
-	   defer file.Close()
+	  defer file.Close()
 
 	   // Bake at n bytes per second until golden brown
 	   _, err = io.WriteString(file, data)
