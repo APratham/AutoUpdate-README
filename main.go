48c815a60c6b8d83c7c46175084ad9709902fa50
APratham <a.pratham1998@gmail.com>
2021-03-24 12:15:50 +0530

    Update main.go

--- main.go
+++ main.go
@@ -47,11 +47,11 @@ func makeReadme(filename string) error {
 
     `
 
-    points := "## About me\n- 🔭 Currently working on Cloud Certifications\n- 🌱 Learning AWS\n- 👯 I'm looking to collaborate on amazing projects as long as they are based on cloud computing\n- 💬 Reach out to me: https://github.com/APratham/APratham/discussions/1\n- 📫 antariksh.pratham@pccoepune.org\n- 😄 Pronouns: He/Him\n"
+    points := "## About me- 🔭 Currently working on Cloud Certifications- 🌱 Learning AWS- 👯 I'm looking to collaborate on amazing projects as long as they are based on cloud computing- 💬 Reach out to me: https://github.com/APratham/APratham/discussions/1- 📫 antariksh.pratham@pccoepune.org- 😄 Pronouns: He/Him"
 
-  	quote := "\n- ⚡ Fun fact: " + lines[fact] + "\n- 🛠️ Currently working on: https://github.com/vigneshshettyin/Flask-Generate-Certificate"
+  	quote := "- ⚡ Fun fact: " + lines[fact] + "- 🛠️ Currently working on: https://github.com/vigneshshettyin/Flask-Generate-Certificate"
     spotify1 := "## I'm currently jamming to"
-    spotify2 := `\n\n<a href="https://spotify-now-playing-iota-umber.vercel.app/now-playing?open"><img src="https://spotify-now-playing-iota-umber.vercel.app/now-playing" width="256" height="64" alt="Now Playing"></a>`
+    spotify2 := `<a href="https://spotify-now-playing-iota-umber.vercel.app/now-playing?open"><img src="https://spotify-now-playing-iota-umber.vercel.app/now-playing" width="256" height="64" alt="Now Playing"></a>`
   	updated := "<sub>Last updated by magic on " + date + ".</sub>"
   	data1 := fmt.Sprintf("%s\n\n%s\n%s\n%s\n\n%s\n\n%s\n\n%s\n\n%s\n", social, title, hello, points, quote, spotify1, spotify2)
     data2 := fmt.Sprintf("%s\n\n", updated)
