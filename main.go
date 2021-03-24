86653f7b88362cb411685c7cdd0b5651bef845ef
APratham <a.pratham1998@gmail.com>
2021-03-24 10:20:19 +0530

    Update main.go

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func makeReadme(filename string) error {
 
     // Whisk together static and dynamic content until stiff peaks form
     const social = `<a href="https://twitter.com/___apratham___" target="_blank" rel="nofollow"><img align="right" alt="Antariksh's Twitter" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg" /></a><a href="https://www.linkedin.com/in/APratham" target="_blank" rel="nofollow"><img align="right" alt="Antariksh's LinkedIn" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg" /></a><a href="https://www.instagram.com/___apratham___" target="_blank" rel="nofollow"><img align="right" alt="Antariksh's Instagram" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/instagram.svg" /></a>`
-    title := "### Hi there 👋"
+    title := "### Hello! I’m Victoria Drake. 👋\n\nI’m a software developer at 💜 and Director of Engineering at work. "
     const hello = `
 
 
