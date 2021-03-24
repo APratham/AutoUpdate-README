665b2259912189cc4602e3726d937d39aa4870fd
APratham <a.pratham1998@gmail.com>
2021-03-24 10:13:25 +0530

    Update main.go

--- main.go
+++ main.go
@@ -37,14 +37,14 @@ func makeReadme(filename string) error {
     const social = `<a href="https://twitter.com/___apratham___" target="_blank" rel="nofollow"><img align="right" alt="Antariksh's Twitter" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg" /></a><a href="https://www.linkedin.com/in/APratham" target="_blank" rel="nofollow"><img align="right" alt="Antariksh's LinkedIn" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg" /></a><a href="https://www.instagram.com/___apratham___" target="_blank" rel="nofollow"><img align="right" alt="Antariksh's Instagram" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/instagram.svg" /></a>`
     const hello = `### Hi there 👋
 
-    <code>
+
                                  __
        ____     ____          __/ /_ __        __
       / _  \   / __ \________/_   _// /_  ____/ /.-..-.
      / __  /  / ____/ __/ _ / /  /_/ __ \/ _ / .-. /, /
     /_/ /_/../_/   /_/ /___/_/____/_/ /_/___/_/  // //
 
-    </code>`
+    `
 
   	quote := "⚡ Fun fact: " + lines[fact]
   	updated := "<sub>Last updated by magic on " + date + ".</sub>"
