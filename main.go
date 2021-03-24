ccf7ae94b55fdc0ff01510a3e0cabcb5bb9230e9
APratham <a.pratham1998@gmail.com>
2021-03-24 11:57:40 +0530

    Update main.go

--- main.go
+++ main.go
@@ -49,8 +49,8 @@ func makeReadme(filename string) error {
 
     points := "## About me\n- 🔭 Currently working on Cloud Certifications\n- 🌱 Learning AWS\n- 👯 I'm looking to collaborate on amazing projects as long as they are based on cloud computing\n- 💬 Reach out to me: https://github.com/APratham/APratham/discussions/1\n- 📫 antariksh.pratham@pccoepune.org\n- 😄 Pronouns: He/Him\n"
 
-  	quote := "\n- ⚡ Fun fact: \n\n" + lines[fact] + "\n- 🛠️ Currently working on: https://github.com/vigneshshettyin/Flask-Generate-Certificate"
-    spotify := `## I'm currently jamming to\n\n<a href="https://spotify-now-playing-iota-umber.vercel.app/now-playing?open"><img src="https://spotify-now-playing-iota-umber.vercel.app/now-playing" width="256" height="64" alt="Now Playing"></a>`
+  	quote := "\n- ⚡ Fun fact: " + lines[fact] + "\n- 🛠️ Currently working on: https://github.com/vigneshshettyin/Flask-Generate-Certificate"
+    spotify := `## I'm currently jamming to" + "<a href="https://spotify-now-playing-iota-umber.vercel.app/now-playing?open"><img src="https://spotify-now-playing-iota-umber.vercel.app/now-playing" width="256" height="64" alt="Now Playing"></a>`
   	updated := "<sub>Last updated by magic on " + date + ".</sub>"
   	data := fmt.Sprintf("%s\n\n%s\n%s\n%s\n\n%s\n\n%s\n\n%s\n", social, title, hello, points, quote, spotify, updated)
 
